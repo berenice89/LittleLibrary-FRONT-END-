d907c6b4d1722941f3606525c21204de81aeeedb
berenice89 <berenice.despres@gmail.com>
2020-07-22 17:45:09 +0200

    started the form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [books, setBooks] = useState([]);
+  const [newBooks, setNewBooks] = useState({});
+  useEffect(() => {
+    axios.get("http://localhost:5050/books").then((res) => {
+      setBooks(res.data);
+    });
+  }, []);
+
+  const handleChange = (e) => {
+    const dataBook = { ...newBooks, [e.target.name]: e.target.value };
+    setNewBooks(dataBook);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(newBooks);
+  };
+
+  // MIN 23 - https://drive.google.com/drive/folders/1Imb2D9lTFrkL5Qndcec9YFeyNTjlxrBq
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Little Library</h1>
+      <h2>My shelves</h2>
+      <ul>
+        {books.map((book) => {
+          return (
+            <li key={book.id}>
+              {book.title}, {book.author}
+            </li>
+          );
+        })}
+      </ul>
+
+      <h2>Add a book</h2>
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
+        <input
+          type="text"
+          name="title"
+          onChange={(e) => {
+            handleChange(e);
+          }}
+        />
+        <input
+          type="text"
+          name="author"
+          onChange={(e) => {
+            handleChange(e);
+          }}
+        />
+        <input
+          type="date"
+          name="date"
+          onChange={(e) => {
+            handleChange(e);
+          }}
+        />
+        <input
+          type="number"
+          name="rate"
+          onChange={(e) => {
+            handleChange(e);
+          }}
+        />
+        <input
+          type="text"
+          name="comment"
+          onChange={(e) => {
+            handleChange(e);
+          }}
+        />
+        <input type="submit" value="ADD" />
+      </form>
     </div>
   );
 }
